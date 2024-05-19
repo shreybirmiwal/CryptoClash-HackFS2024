74e5654657850f83996f839399855d1d85c37e24
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-05-19 12:11:12 -0500

    photon and main menu

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting to Master");
+        PhotonNetwork.ConnectUsingSettings();
+    } 
+
+
+    public override void OnConnectedToMaster(){
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby(){
+        Debug.Log("Joined Lobby");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
