3f0d3bea2fdab8f9fc1f041f86e341c74fd6f67d
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-05-21 11:18:42 -0500

    join games and button '

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -5,40 +5,34 @@ using Photon.Pun;
 using TMPro;
 using Thirdweb;
 
-
 public class Launcher : MonoBehaviourPunCallbacks
 {
-
     //menus
     public GameObject loadingMenu;
     public GameObject mainMenu;
     public GameObject createMenu;
     public GameObject inventoryMenu;
-
+    public GameObject joinedMenu;
+    public GameObject joinMenu;
 
     public TMP_InputField roomNameInput;
     public TMP_InputField roomBetAmountInput;
     private string map = "Spiky Sallon";
 
-
     public TMP_Text errorTextCreateRoom;
+    public TMP_Text gameRoomName_joinedMenu;
 
     private ThirdwebSDK sdk;
-
 
     // Start is called before the first frame update
     void Start()
     {
-
         //connect to thirdweb
         sdk = ThirdwebManager.Instance.SDK;
-
-
 
         Debug.Log("Connecting to Master");
         PhotonNetwork.ConnectUsingSettings();
     }
-
 
     public override void OnConnectedToMaster()
     {
@@ -49,20 +43,9 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         Debug.Log("Joined Lobby");
-
-        loadingMenu.SetActive(false);
-        mainMenu.SetActive(true);
-        createMenu.SetActive(false);
-        inventoryMenu.SetActive(false);
+        ShowMenu(mainMenu);
     }
 
-    public void ClickCreateRoom()
-    {
-        mainMenu.SetActive(false);
-        loadingMenu.SetActive(false);
-        createMenu.SetActive(true);
-        inventoryMenu.SetActive(false);
-    }
 
 
     public async void CreateRoom()
@@ -73,7 +56,6 @@ public class Launcher : MonoBehaviourPunCallbacks
             return;
         }
 
-
         //wallet
         var data = await sdk.Wallet.IsConnected();
         if (data == false)
@@ -82,24 +64,17 @@ public class Launcher : MonoBehaviourPunCallbacks
             return;
         }
 
-
         PhotonNetwork.CreateRoom(roomNameInput.text, new Photon.Realtime.RoomOptions { MaxPlayers = 2 });
 
-        mainMenu.SetActive(false);
-        loadingMenu.SetActive(true);
-        createMenu.SetActive(false);
-        inventoryMenu.SetActive(false);
+        ShowMenu(loadingMenu);
     }
-
 
     public override void OnJoinedRoom()
     {
         Debug.Log("Joined Room with " + PhotonNetwork.CurrentRoom.PlayerCount + " players" + "room name : " + PhotonNetwork.CurrentRoom.Name);
-
-
-
+        gameRoomName_joinedMenu.text = PhotonNetwork.CurrentRoom.Name;
+        ShowMenu(joinedMenu);
     }
-
 
     public void handleDropDownUpdateMapSelect(int val)
     {
@@ -107,7 +82,6 @@ public class Launcher : MonoBehaviourPunCallbacks
         if (val == 0) map = "Spiky Sallon";
         if (val == 1) map = "1";
         if (val == 2) map = "2";
-
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -115,24 +89,15 @@ public class Launcher : MonoBehaviourPunCallbacks
         Debug.Log("Create Room Failed: " + message);
     }
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-    public void openInventory()
-    {
-        mainMenu.SetActive(false);
-        loadingMenu.SetActive(false);
-        createMenu.SetActive(false);
-        inventoryMenu.SetActive(true);
-    }
 
-    public void backToMainMenu()
+    private void ShowMenu(GameObject menuToShow)
     {
-        mainMenu.SetActive(true);
-        loadingMenu.SetActive(false);
-        createMenu.SetActive(false);
-        inventoryMenu.SetActive(false);
+        loadingMenu.SetActive(menuToShow == loadingMenu);
+        mainMenu.SetActive(menuToShow == mainMenu);
+        createMenu.SetActive(menuToShow == createMenu);
+        inventoryMenu.SetActive(menuToShow == inventoryMenu);
+        joinedMenu.SetActive(menuToShow == joinedMenu);
+        joinMenu.SetActive(menuToShow == joinMenu);
     }
 
     void Update()
@@ -141,6 +106,39 @@ public class Launcher : MonoBehaviourPunCallbacks
     }
 
 
+
+
+
+
+
+
+
+
+
+
+    ///button clicks
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void openInventory()
+    {
+        ShowMenu(inventoryMenu);
+    }
+
+    public void ClickCreateRoom()
+    {
+        ShowMenu(createMenu);
+    }
+    public void backToMainMenu()
+    {
+        ShowMenu(mainMenu);
+    }
+    public void ClickJoinRoom()
+    {
+        ShowMenu(joinMenu);
+    }
 
 
 }
