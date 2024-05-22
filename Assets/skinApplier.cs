6b972ec0876a5fc9029729efe193ce21c984345e
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-05-21 23:02:17 -0500

    COMPLETLY LOCAL SKIN AND WEAPON SELECT, WORKIGN ON SYNCRONIZE, https://www.youtube.com/watch?v=b1RygpoGTuA&list=PLhsVv9Uw1WzjI8fEBjBQpTyXNZ6Yp1ZLw&index=9

--- Assets/skinApplier.cs
+++ Assets/skinApplier.cs
@@ -10,6 +10,7 @@ using System.Text.RegularExpressions;
 public class skinApplier : MonoBehaviour
 {
 
+    public PhotonView photonView;
 
     public List<GameObject> skins;
     public List<GameObject> weapons;
@@ -19,45 +20,59 @@ public class skinApplier : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string nick = PhotonNetwork.NickName;
-        if (nick.Contains("*"))
+
+        if (!photonView.IsMine)
         {
-            // Code to handle the case when nick contains *
-            string pattern = @"\d+";
-            MatchCollection matches = Regex.Matches(nick, pattern);
-
-
-            string firstNumber = matches[0].Value;
-            string secondNumber = matches[1].Value;
-
-            string username = nick.Substring(nick.IndexOf("*") + 1);
-
-            Debug.Log("FIRST NUMBER IS " + firstNumber);
-            Debug.Log("SECOND NUMBER IS " + secondNumber);
-            Debug.Log("USERNAME IS " + username);
-
-
-            int weaponIndex = int.Parse(firstNumber);
-            int skinIndex = int.Parse(secondNumber);
-
-            foreach (var skin in skins)
-            {
-                skin.SetActive(false);
-            }
-            skins[skinIndex].SetActive(true);
-
-            foreach (var weapon in weapons)
-            {
-                weapon.SetActive(false);
-            }
-            weapons[weaponIndex].SetActive(true);
-
+            return;
         }
         else
         {
-            Debug.Log("USE DEFUALTS");
-            Debug.Log("NAME IS " + nick);
-            // Use defaults
+
+            string nick = PhotonNetwork.NickName;
+            if (nick.Contains("*"))
+            {
+                // Code to handle the case when nick contains *
+                string pattern = @"\d+";
+                MatchCollection matches = Regex.Matches(nick, pattern);
+
+
+                string firstNumber = matches[0].Value;
+                string secondNumber = matches[1].Value;
+
+                string username = nick.Substring(nick.IndexOf("*") + 1);
+
+                int weaponIndex = int.Parse(firstNumber);
+                int skinIndex = int.Parse(secondNumber);
+
+                foreach (var skin in skins)
+                {
+                    skin.SetActive(false);
+                }
+                skins[skinIndex].SetActive(true);
+
+                foreach (var weapon in weapons)
+                {
+                    weapon.SetActive(false);
+                }
+                weapons[weaponIndex].SetActive(true);
+
+            }
+            else
+            {
+                // Use defaults
+                foreach (var skin in skins)
+                {
+                    skin.SetActive(false);
+                }
+                skins[0].SetActive(true);
+
+                foreach (var weapon in weapons)
+                {
+                    weapon.SetActive(false);
+                }
+                weapons[0].SetActive(true);
+
+            }
         }
     }
 
