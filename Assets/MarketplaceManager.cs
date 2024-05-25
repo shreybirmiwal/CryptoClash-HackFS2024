10cb65e55d886280849f775a489600ede01cab1d
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-05-24 23:27:26 -0500

    working, just needs styling and purchase button

--- Assets/MarketplaceManager.cs
+++ Assets/MarketplaceManager.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.UI;
 using Newtonsoft.Json;
+using TMPro;
+using UnityEngine.UI;
 
 public class MarketplaceManager : MonoBehaviour
 {
-    public string projectID = "0e014a1b-1024-4027-a253-0138e863480e";
-    public string apiURL = "https://api.gaming.chainsafe.io/v1/projects/";
+    public string projectID;
+    public string marketplaceID;
+    private string apiURL = "https://api.gaming.chainsafe.io/v1/projects/";
     public GameObject itemTemplate;
     public Transform contentTransform;
 
     void Start()
     {
+        Debug.Log("Marketplace opened");
         StartCoroutine(GetMarketplaceItems());
     }
 
     IEnumerator GetMarketplaceItems()
     {
-        string url = apiURL + projectID + "/items?chainId=1"; // Assuming chainId is 1 for example
+        string url = $"{apiURL}{projectID}/marketplaces/{marketplaceID}/items";
         UnityWebRequest request = UnityWebRequest.Get(url);
         yield return request.SendWebRequest();
 
@@ -30,6 +33,7 @@ public class MarketplaceManager : MonoBehaviour
         else
         {
             string jsonResponse = request.downloadHandler.text;
+            Debug.Log(jsonResponse);
             ItemsResponse itemsResponse = JsonConvert.DeserializeObject<ItemsResponse>(jsonResponse);
             PopulateItems(itemsResponse.items);
         }
@@ -40,10 +44,31 @@ public class MarketplaceManager : MonoBehaviour
         foreach (var item in items)
         {
             GameObject itemGO = Instantiate(itemTemplate, contentTransform);
-            itemGO.transform.Find("ItemName").GetComponent<Text>().text = item.token.metadata["name"].ToString();
-            itemGO.transform.Find("ItemPrice").GetComponent<Text>().text = item.price;
-            Button purchaseButton = itemGO.transform.Find("PurchaseButton").GetComponent<Button>();
-            purchaseButton.onClick.AddListener(() => PurchaseItem(item.id));
+            TMP_Text nameText = itemGO.transform.Find("ItemName").GetComponent<TMP_Text>();
+            TMP_Text priceText = itemGO.transform.Find("ItemPrice").GetComponent<TMP_Text>();
+            Image itemImage = itemGO.transform.Find("ItemImage").GetComponent<Image>();
+            Button purchaseBtn = itemGO.transform.Find("PurchaseButton").GetComponent<Button>();
+
+            nameText.text = item.token.metadata["name"].ToString();
+            priceText.text = item.price;
+            StartCoroutine(LoadImage(item.token.metadata["image"].ToString(), itemImage));
+            purchaseBtn.onClick.AddListener(() => PurchaseItem(item.id));
+        }
+    }
+
+    IEnumerator LoadImage(string imageUrl, Image itemImage)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(imageUrl);
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(request.error);
+        }
+        else
+        {
+            Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            itemImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
         }
     }
 
