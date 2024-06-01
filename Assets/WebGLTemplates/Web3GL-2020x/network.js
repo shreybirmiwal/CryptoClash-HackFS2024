b47e78172dcd0e8b62b42ea3d96a6c20b7f11313
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-06-01 00:26:40 -0500

    ^

---
+++ Assets/WebGLTemplates/Web3GL-2020x/network.js
@@ -0,0 +1,9 @@
+//You can see a list of compatible EVM chains at https://chainlist.org/
+window.networks = [
+  {
+    id: 11155111,
+    label: "Ethereum Sepolia",
+    token: "Seth",
+    rpcUrl: 'https://rpc.sepolia.org',
+  }
+]
