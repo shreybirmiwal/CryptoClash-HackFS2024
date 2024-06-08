5f7f231866e890477e79e6e017fb5ba8c7cd4a08
Shrey birmiwal <shreybirmiwal@gmail.com>
2024-06-08 17:27:30 -0500

    can only join game once logged in!

---
+++ Assets/COPY_CHAINSAFE/LoadSceneOnLogin.cs
@@ -0,0 +1,51 @@
+using ChainSafe.Gaming.UnityPackage.Common;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace ChainSafe.Gaming.UnityPackage.Common
+{
+    /// <summary>
+    /// Loads scene when Web3 Instance is initialized.
+    /// </summary>
+    public class LoadSceneOnLogin : MonoBehaviour, IWeb3InitializedHandler
+    {
+
+
+
+
+        public GameObject connectWalletText;
+        public Button startNew;
+        public Button joinGame;
+        public Button inventory;
+        public Button marketplace;
+
+
+
+
+
+        /// <summary>
+        /// Login scene cached/saved for Logout since we can have more than one Login scene.
+        /// </summary>
+
+        public void Start()
+        {
+            connectWalletText.SetActive(true);
+            startNew.interactable = false;
+            joinGame.interactable = false;
+            inventory.interactable = false;
+            marketplace.interactable = false;
+
+        }
+
+        public void OnWeb3Initialized()
+        {
+            connectWalletText.SetActive(false);
+            startNew.interactable = true;
+            joinGame.interactable = true;
+            inventory.interactable = true;
+            marketplace.interactable = true;
+
+        }
+    }
+}
